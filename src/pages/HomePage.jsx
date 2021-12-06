d8d688c5e2cb2101cfc53c3ca3e9d5e22ed71b07
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-06 14:45:57 +0100

    Add categories component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
-import "../App.css";
+import "../App.css"
+
+import Categories from "../components/Categories/Categories.jsx"
+import Searchbar from "../components/Searchbar/Searchbar";
+
 
 function HomePage() {
   return (
-    <div className="App">
-
+    <div className="App container">
+      <h1>What do you want to eat today?</h1>
+        <Searchbar />
+        <Categories />
     </div>
   );
 }
