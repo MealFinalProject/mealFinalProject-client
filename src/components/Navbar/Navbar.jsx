d0c4f272303a4f08b62c9ab6e4c7f8d765173ecc
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-06 14:45:28 +0100

    Modify navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = (props) => {
   return (
     <nav>
       <Link to={PATHS.HOMEPAGE} className="nav__projectName">
-        {CONSTS.CAPITALIZED_APP} - created with IronLauncher
+        Project!
       </Link>
 
       <div className="nav__authLinks">
