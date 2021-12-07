7736fc14ca695c4b820564fc270ee851c5a1b6be
Aaron <n_aaron5@hotmail.com>
2021-12-07 21:31:34 +0100

    work in categories component, insert modal

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,12 +3,34 @@ import './Categories.css'
 const Categories = (props) => {
 
     const { text, img } = props
-
-    const style  = {}
+    
+    const style  = {
+        backgroundImage: 'url('+ `${img}` + ')',
+        backgroundSize: 'cover',
+    }
 
     return(
-          <div className="Categories d-flex align-items-end" style={}>
-            <p className="text-center">{text}</p>
+          <div className="Categories d-flex align-items-end" style={style}>
+
+            <button type="button" data-toggle="modal" data-target="#exampleModal">
+                <p className=" m-2 text-white font-weight-bold">{text}</p>
+            </button>
+                        
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{text}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                </div>
+            </div>
+            </div>
           </div>
     )
 }
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -14,9 +14,9 @@ function HomePage() {
           <Searchbar />
           <div className="d-flex justify-content-around flex-wrap">
             <Categories text={'Type of food (Country)'} img={'https://www.lux-review.com/wp-content/uploads/2020/03/Pasta-1.jpg'}/>
-            <Categories />
-            <Categories />
-            <Categories />
+            <Categories text={'According to time of day'} img={'https://estaticos.miarevista.es/media/cache/1140x_thumb/uploads/images/article/5feb33bd5bafe860ee3e874f/captura-de-pantalla-2020-12-29-a-las-14-51-14_0.jpg'}/>
+            <Categories text={'Something fast?'} img={'https://assets.aboutkidshealth.ca/AKHAssets/fast_food_better_choices.jpg?renditionid=21'}/>
+            <Categories text={"What's left in the fridge?"} img={'https://www.gannett-cdn.com/presto/2020/08/04/USAT/0014f6e5-9efe-47e5-a31e-259ff584e567-Fridge1.jpeg?crop=727,545,x123,y2&quality=50&width=640'}/>
           </div>
       </div>
       
