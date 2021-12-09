ae4e6ad60bd2101f1b914ed50e5189abb24b0e5c
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-09 17:47:55 +0100

    Delete warnings for class instead of classname

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -12,86 +12,82 @@ const RecipeResults = () => {
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const API_URL = `${process.env.REACT_APP_SERVER_URL}/search/${id}`;
-  console.log(API_URL);
   useEffect(() => {
     axios.get(API_URL).then((response) => {
-      console.log("response.data", response.data);
       setRecipe(response.data);
       setLoading(false);
-      console.log("Hola");
     });
   }, []);
-//   const JSONrecipeTest = {
-//     uri: "http://www.edamam.com/ontologies/edamam.owl#recipe_aee621fd197a61c324a15fec5d338802",
-//     label: "Perfect Grilled Chicken recipes",
-//     image:
-//       "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0",
-//     images: {
-//       THUMBNAIL: {
-//         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0-s",
-//         width: 100,
-//         height: 100,
-//       },
-//       SMALL: {
-//         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0-m",
-//         width: 200,
-//         height: 200,
-//       },
-//       REGULAR: {
-//         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0",
-//         width: 300,
-//         height: 300,
-//       },
-//     },
-//     source: "fashionablefoods.com",
-//     url: "http://fashionablefoods.com/2015/05/19/perfect-grilled-chicken/",
-//     shareAs:
-//       "http://www.edamam.com/recipe/perfect-grilled-chicken-recipes-aee621fd197a61c324a15fec5d338802/-",
-//     yield: 4,
-//     dietLabels: [],
-//     healthLabels: [],
-//     cautions: [],
-//     ingredientLines: [
-//       "2 Large Chicken Breasts",
-//       "1 Tablespoon Oil (Olive Oil, Melted Coconut Oil, etc.)",
-//       "3 Tablespoons Herb or Spice Rub*",
-//     ],
-//     ingredients: [],
-//     calories: 894.316,
-//     totalWeight: 457.1,
-//     totalTime: 20,
-//     cuisineType: ["american"],
-//     mealType: ["lunch", "dinner"],
-//     dishType: ["main course"],
-//     totalNutrients: {},
-//     totalDaily: {},
-//     digest: [],
-//     id: "aee621fd197a61c324a15fec5d338802",
-//   };
-//   const {
-//     label,
-//     image,
-//     ingredientLines,
-//     calories,
-//     totalTime,
-//     cuisineType,
-//     mealType,
-//     dishType,
-//     url,
-//   } = JSONrecipeTest;
-    const {
-      label,
-      image,
-      ingredientLines,
-      calories,
-      totalTime,
-      cuisineType,
-      mealType,
-      dishType,
-      url
-    } = recipe?.recipe ?? {};
-    const serves = recipe?.recipe?.yield ?? {}
-  console.log(recipe);
+  //   const JSONrecipeTest = {
+  //     uri: "http://www.edamam.com/ontologies/edamam.owl#recipe_aee621fd197a61c324a15fec5d338802",
+  //     label: "Perfect Grilled Chicken recipes",
+  //     image:
+  //       "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0",
+  //     images: {
+  //       THUMBNAIL: {
+  //         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0-s",
+  //         width: 100,
+  //         height: 100,
+  //       },
+  //       SMALL: {
+  //         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0-m",
+  //         width: 200,
+  //         height: 200,
+  //       },
+  //       REGULAR: {
+  //         url: "https://www.edamam.com/web-img/c60/c60dbe071bde7d54be9a78af89dfe8a0",
+  //         width: 300,
+  //         height: 300,
+  //       },
+  //     },
+  //     source: "fashionablefoods.com",
+  //     url: "http://fashionablefoods.com/2015/05/19/perfect-grilled-chicken/",
+  //     shareAs:
+  //       "http://www.edamam.com/recipe/perfect-grilled-chicken-recipes-aee621fd197a61c324a15fec5d338802/-",
+  //     yield: 4,
+  //     dietLabels: [],
+  //     healthLabels: [],
+  //     cautions: [],
+  //     ingredientLines: [
+  //       "2 Large Chicken Breasts",
+  //       "1 Tablespoon Oil (Olive Oil, Melted Coconut Oil, etc.)",
+  //       "3 Tablespoons Herb or Spice Rub*",
+  //     ],
+  //     ingredients: [],
+  //     calories: 894.316,
+  //     totalWeight: 457.1,
+  //     totalTime: 20,
+  //     cuisineType: ["american"],
+  //     mealType: ["lunch", "dinner"],
+  //     dishType: ["main course"],
+  //     totalNutrients: {},
+  //     totalDaily: {},
+  //     digest: [],
+  //     id: "aee621fd197a61c324a15fec5d338802",
+  //   };
+  //   const {
+  //     label,
+  //     image,
+  //     ingredientLines,
+  //     calories,
+  //     totalTime,
+  //     cuisineType,
+  //     mealType,
+  //     dishType,
+  //     url,
+  //   } = JSONrecipeTest;
+  const {
+    label,
+    image,
+    ingredientLines,
+    calories,
+    totalTime,
+    cuisineType,
+    mealType,
+    dishType,
+    url,
+  } = recipe?.recipe ?? {};
+  const serves = recipe?.recipe?.yield ?? {};
   return (
     !loading && (
       <div className="container d-flex flex-wrap justify-content-center">
@@ -102,18 +98,20 @@ const RecipeResults = () => {
           <p className="fw-bold fs-2">{label}</p>
         </div>
         <div className="m-2">
-          {totalTime !== 0 && <span className="m-2">
-            <i class="bi bi-clock-history"></i> {totalTime} mins
-          </span>}
+          {totalTime !== 0 && (
+            <span className="m-2">
+              <i className="bi bi-clock-history"></i> {totalTime} mins
+            </span>
+          )}
           <span className="m-2">
-            <i class="bi bi-activity"></i> {Math.round(calories)} kcal
+            <i className="bi bi-activity"></i> {Math.round(calories)} kcal
           </span>
           <span className="m-2">
-            <i class="bi bi-people-fill">{serves} serves</i>
+            <i className="bi bi-people-fill">{serves} serves</i>
           </span>
         </div>
         <div className="m-2">
-        <span className="fw-bold">Dish type </span>
+          <span className="fw-bold">Dish type </span>
           {dishType.map((dish) => (
             <span className="m-2 text-capitalize">{dish}</span>
           ))}
@@ -128,10 +126,10 @@ const RecipeResults = () => {
         </div>
         <div className="m-2 mt-3 ">
           <a
-            className="border rounded-pill p-2 text-light type-background"
+            className="border rounded p-2 text-light type-background"
             href={url}
           >
-            Instructions
+            <i className="bi bi-file-text"></i> Instructions
           </a>
         </div>
 
