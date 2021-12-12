fd2deaf391c0f9d293ba4881dfd23101b600a6e4
Aarón López <n_aaron5@hotmail.com>
2021-12-12 18:48:35 +0100

    update profile load image change username and password

--- src/pages/UpdateProfileInfo/UpdateProfileInfo.jsx
+++ src/pages/UpdateProfileInfo/UpdateProfileInfo.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useCallback } from "react";
 
 import './UpddateProfileInfo.css'
 
@@ -10,8 +10,10 @@ const UpdateProfileInfo = (props) => {
 
     const { user, } = props
     const [imageSelected, setImageSelected] = useState('')
+    const [profileImage, setProfileImage]   = useState()
     const [imageLoaded, setImageLoaded]     = useState(false)
-    let profileImage = ''
+    const [newUsername, setNewUsername]     = useState()
+    const [newPassword, setNewPassword]     = useState()
     console.log(user)
     
     const uploadImage = async () => {
@@ -21,7 +23,7 @@ const UpdateProfileInfo = (props) => {
 
     await axios.post("https://api.cloudinary.com/v1_1/djosvkjof/image/upload", 
     formData).then((response)=>{
-      profileImage = response.data.public_id
+      setProfileImage(response.data.public_id)
       setImageLoaded(true)
       console.log(response)
       
@@ -29,28 +31,39 @@ const UpdateProfileInfo = (props) => {
     
   }
 
-  const updateInfo = (event) => {
+  const updateInfo = async (event) => {
     event.preventDefault()
-    axios
-    .post(`${process.env.REACT_APP_SERVER_URL}/profile/update`, {
+    await axios.post(`${process.env.REACT_APP_SERVER_URL}/profile/update`, {
       data: {
         profileImage: profileImage,
         userId: user._id,
+        newUsername: newUsername,
+        newPassword: newPassword,
       }
-    })
-    .then((response) => {
+    }).then((response) => {
+      
+      console.log(response)
     })
   }
-  
     return(
         <div className="UpdateProfileInfo">
-          <p>Upload profile image:</p>
-            <input id="input-files" className="mt-4" type="file" onChange={(event)=>{setImageSelected(event.target.files[0])}} />
+          
+            <div className="form-group mb-3 mt-5">
+                <p className="font-weight-bold">Enter new Username:</p>
+                <input className="form-control" type="text" name="username" placeholder="Username" onChange={(event) => setNewUsername(event.target.value)}/>
+            </div>
+            <div>
+                <p className="font-weight-bold">Enter new Password:</p>
+                <input className="form-control mb-2" type="password" name="password" placeholder="Password"  onChange={(event) => setNewPassword(event.target.value)} minLength="8" /> 
+            </div>
+            <p className="mt-4">Upload profile image:</p>
+            <input id="input-files" className="mt-1" type="file" onClick={() => setImageLoaded(false)} onChange={(event)=>{setImageSelected(event.target.files[0])}} />
             <button className=" btn btn-block mybtn bg-color-purple tx-tfm mb-2 mt-4"  onClick={uploadImage}>Upload image</button>
-            {imageLoaded && <p>Image loaded successfully</p>}
+          {imageLoaded && <p>Image loaded successfully</p>}
             <button 
             className=" btn btn-block mybtn bg-color-purple tx-tfm mb-2 mt-4" 
             onClick={(event) => updateInfo(event)}>Update Info</button>
+         
         </div>
     )
 }
