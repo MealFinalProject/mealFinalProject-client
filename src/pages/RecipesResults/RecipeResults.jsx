f74b0b7643e2549cb8f5778cbaf2925e32e1db7c
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-16 19:21:36 +0100

    [Front] Recipes result - Mostrar que no hay resultados

--- src/pages/RecipesResults/RecipeResults.jsx
+++ src/pages/RecipesResults/RecipeResults.jsx
@@ -11,11 +11,12 @@ import { Link } from "react-router-dom";
 
 const RecipeResults = (props) => {
   const [recipes, setRecipes] = useState([]);
+  const [noResults, setNoResults] = useState(false);
   const { type, name } = useParams();
   const { searchState, setSearchState, user, handleLogout, profileImageState } = props;
 
   let API_URL = `${process.env.REACT_APP_SERVER_URL}/category/${type}`;
-  
+
   if (name) API_URL += `/${name}`;
   useEffect(() => {
     if (searchState) {
@@ -24,34 +25,40 @@ const RecipeResults = (props) => {
       setSearchState("");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchState])
+  }, [searchState]);
 
   useEffect(() => {
     axios.get(API_URL).then((response) => {
       setRecipes(response.data);
+      if(recipes.length === 0) setNoResults(true)
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="mt-5">
-      <Navbar handleLogout={handleLogout} user={user} profileImageState={profileImageState} />
+      <Navbar
+        handleLogout={handleLogout}
+        user={user}
+        profileImageState={profileImageState}
+      />
       <div className="container d-flex flex-wrap justify-content-center">
-    
-      {recipes.map((element, index) => {
-        return (
-             <Link key={index + 1} to={`/search/${element.recipe.id}`}>
-                <Category
-                  key={element.recipe.id}
-                  text={element.recipe.label}
-                  img={element.recipe.image}
-                />
-              </Link>
+        <div className="mt-5">
+          {!noResults && <p>Oops nothing seems to have been found. <Link to={"/"}>Maybe you want to try again?</Link></p>}
+        </div>
+        {recipes.map((element, index) => {
+          return (
+            <Link key={index + 1} to={`/search/${element.recipe.id}`}>
+              <Category
+                key={element.recipe.id}
+                text={element.recipe.label}
+                img={element.recipe.image}
+              />
+            </Link>
           );
-            })}
+        })}
+      </div>
     </div>
-    </div>
-    
   );
 };
 
