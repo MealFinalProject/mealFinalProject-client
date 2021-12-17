00364a5f0a2f88b900f93bfcc03aa59c9ef04d95
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-17 18:08:48 +0100

    Style sign up

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -53,67 +53,54 @@ export default function Signup({ authenticate }) {
  
 
   return (
-    // <div>
-    //   <h1>Sign Up</h1>
-    //   <form onSubmit={handleFormSubmission} className="auth__form">
-    //     <label htmlFor="input-username">Username</label>
-    //     <input
-    //       id="input-username"
-    //       type="text"
-    //       name="username"
-    //       placeholder="Text"
-    //       value={username}
-    //       onChange={handleInputChange}
-    //       required
-    //     />
-
-    //     <label htmlFor="input-password">Password</label>
-    //     <input
-    //       id="input-password"
-    //       type="password"
-    //       name="password"
-    //       placeholder="Password"
-    //       value={password}
-    //       onChange={handleInputChange}
-    //       required
-    //       minLength="8"
-    //     />
-
-    //     {error && (
-    //       <div className="error-block">
-    //         <p>There was an error submiting the form:</p>
-    //         <p>{error.message}</p>
-    //       </div>
-    //     )}
-
-    //     <button className="button__submit" type="submit">
-    //       Submit
-    //     </button>
-    //   </form>
-    // </div>
-    <div className="container d-flex flex-column justify-content-center">
-    <h1 className="text-center m-5">Project Meal</h1>
-      <div className="Sign-up d-flex justify-content-center align-items-center">
-      <form onSubmit={handleFormSubmission} className="mb-2 Login" >
-                <div className="form-group mb-3">
-                    <p className="font-weight-bold text-white">Enter Username:</p>
-                    <input onClick={() => setErrorMessage(null)} className="form-control" type="text" name="username" placeholder="Username"value={username} onChange={handleInputChange} required />
-                </div>
-                <div>
-                    <p className="font-weight-bold text-white">Enter Password:</p>
-                    <input onClick={() => setErrorMessage(null)} className="form-control mb-2" type="password" name="password" placeholder="Password"  value={password} onChange={handleInputChange} required minLength="8" />
-                    
-                </div>
-
-                <div className="col-md-12 text-center mt-4">
-                    <button type="submit" className=" btn btn-block mybtn bg-color tx-tfm">Sign up</button>
-                </div>
-                <div>
-                <p className="text-center text-white font-weight-bold mt-5">Already have an account? <Link className="text-decoration-none font-weight-bold color-log-in" to={PATHS.LOGINPAGE} >Log in</Link></p>
-                </div>
+    <>
+      <div className="row p-0 m-0 container-fluid justify-content-center mt-xl-5">
+        <div className="col-12 col-xl-6 p-0 mb-3 text-end">
+          <img id="login-image" src="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" alt="Log in"/>
+        </div>
+        <div className="col-12 col-xl-6 p-0 justify-content-start mt-xl-3">
+            <form onSubmit={handleFormSubmission} className="col-12 col-xl-6 mb-2 Login" >
+              <div className="form-group mb-3 col-12">
+                <p className="font-weight-bold">Enter Username:</p>
+                <input onClick={() => setErrorMessage(null)} className="form-control" type="text" name="username" placeholder="Username"value={username} onChange={handleInputChange} required />
+              </div>
+              <div className="form-group mb-3 col-12">
+                <p className="font-weight-bold">Enter Password:</p>
+                <input onClick={() => setErrorMessage(null)} className="form-control mb-2" type="password" name="password" placeholder="Password"  value={password} onChange={handleInputChange} required minLength="8" />
+              </div>
+              <div className="form-group col-12 text-center mt-4">
+                  <button type="submit" className=" btn btn-block mybtn bg-color tx-tfm">Sign up</button>
+              </div>
+              <div className="form-group col-12">
+                <p className="text-center font-weight-bold mt-5">Already have an account? <Link className="text-decoration-none font-weight-bold color-log-in" to={PATHS.LOGINPAGE} >Log in</Link></p>
+              </div>
             </form>
+        </div>
       </div>
-      {errorMessage && <p className="text-center text-danger">{errorMessage}</p>}
-    </div>
+      {/* <div className="container d-flex flex-column justify-content-center">
+        <h1 className="text-center m-5">Project Meal</h1>
+        <div className="Sign-up d-flex justify-content-center align-items-center">
+        <form onSubmit={handleFormSubmission} className="mb-2 Login" >
+                  <div className="form-group mb-3">
+                      <p className="font-weight-bold text-white">Enter Username:</p>
+                      <input onClick={() => setErrorMessage(null)} className="form-control" type="text" name="username" placeholder="Username"value={username} onChange={handleInputChange} required />
+                  </div>
+                  <div>
+                      <p className="font-weight-bold text-white">Enter Password:</p>
+                      <input onClick={() => setErrorMessage(null)} className="form-control mb-2" type="password" name="password" placeholder="Password"  value={password} onChange={handleInputChange} required minLength="8" />
+                      
+                  </div>
+
+                  <div className="col-md-12 text-center mt-4">
+                      <button type="submit" className=" btn btn-block mybtn bg-color tx-tfm">Sign up</button>
+                  </div>
+                  <div>
+                  <p className="text-center text-white font-weight-bold mt-5">Already have an account? <Link className="text-decoration-none font-weight-bold color-log-in" to={PATHS.LOGINPAGE} >Log in</Link></p>
+                  </div>
+              </form>
+        </div>
+        {errorMessage && <p className="text-center text-danger">{errorMessage}</p>}
+      </div> */}
+    </>
   );
 }
