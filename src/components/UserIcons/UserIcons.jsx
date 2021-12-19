c7a40bb4e63815021c9e0c103df71792ee823bbc
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-19 13:37:57 +0100

    Fix bug with missing field in db

--- src/components/UserIcons/UserIcons.jsx
+++ src/components/UserIcons/UserIcons.jsx
@@ -2,15 +2,20 @@ import './UserIcons.css'
 
 const UserIcons = ( {userFromDB} ) => {
 
+    const followers = (userFromDB.followers ? userFromDB.followers.length : 0)
+    const followed = (userFromDB.followed ? userFromDB.followed.length : 0)
+    const favs = (userFromDB.favs_recipes ? userFromDB.favs_recipes.length : 0)
+    const comments = (userFromDB.comments ? userFromDB.comments.length : 0)
+
     return(
         <div className='d-flex align-items-center'>
             <div className='UserIcons container d-flex justify-content-start'>
-                <p className='mr-5'>Followers{userFromDB.followers.length !== 0 && userFromDB.followers.length}</p>
-                <p>Followed {userFromDB.followed.length !== 0 && userFromDB.followed.length}</p>
+                <p className='mr-5'>Followers {followers}</p>
+                <p>Following {followed}</p>
             </div>
             <div className='UserIcons container d-flex justify-content-end'>
-                <i id="favsIcon" className="bi bi-heart">  {userFromDB.favs_recipes.length !== 0 && userFromDB.favs_recipes.length}</i>
-                <i id="commentsIcon"  className="far fa-comment" >{userFromDB.comments.length !== 0 && userFromDB.comments.length}</i>
+                <i id="favsIcon" className="bi bi-heart">  {favs}</i>
+                <i id="commentsIcon"  className="far fa-comment" > {comments}</i>
             </div> 
         </div>
            
