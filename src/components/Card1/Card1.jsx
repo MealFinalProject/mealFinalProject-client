0437cd25519f8ef9ff3cba2dd16631df9134f555
Aarón López <n_aaron5@hotmail.com>
2021-12-23 09:53:35 +0100

    changes

--- src/components/Card1/Card1.jsx
+++ src/components/Card1/Card1.jsx
@@ -10,7 +10,7 @@ const Card1 = (props) => {
 
   return (
     <div className="d-flex justify-content-center">
-       <div>
+       <div className="d-flex justify-content-center">
         <div className="Card1 d-flex align-items-end m-0 " style={style}>
           <p className=" m-2 text-left text-white font-weight-bold">{text}</p>
         </div>
